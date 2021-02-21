b6641225b777f23eb0ba2326561064fe67c2bc90
Jack Miller <jackmiller23530@gmail.com>
2021-02-20 19:11:24 -0500

    form filler test

--- form_filler.py
+++ form_filler.py
@@ -14,3 +14,9 @@ def fill_out_text_module(module):
 def click_button_modules():
     for i in range(1,6):
         web.find_element_by_xpath(form_inputs.button_xpaths[i]).click()
+
+fill_out_text_module('email')
+fill_out_text_module('first_teacher_monday')
+fill_out_text_module('name')
+click_button_modules()
+web.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[3]/div[3]/div/div').click()
